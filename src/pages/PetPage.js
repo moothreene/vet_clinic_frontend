9bdc7246550276ae2809eb7d9025591b5e84392d
Martin <moothreene@gmail.com>
2024-03-01 18:53:34 +0300

    pet page updated view

--- src/pages/PetPage.js
+++ src/pages/PetPage.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link, Navigate, useParams } from 'react-router-dom'
+import { Link, Navigate, json, useParams } from 'react-router-dom'
 import Manipulation from '../components/Manipulation';
 import { useSelector } from 'react-redux';
 
@@ -9,6 +9,7 @@ function PetPage() {
     const [petData,setPetData] = useState([]);
     const [manipulations, setManipulations] = useState([]);
     const userData = useSelector(state=>state.user);
+
     useEffect(()=>{
         if (userData.user){
             axios.get(`http://localhost:5000/pet/${petId}`,{withCredentials:true}).then(
@@ -19,19 +20,29 @@ function PetPage() {
         )
         }
     },[]);
+
+    const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+
     if (!userData.user) return <Navigate to="/" />
     return (
         <div>
             <h1>{petData.name}</h1>
-            {JSON.stringify(petData)}
+            <div>{getAge(petData.birthday)} y.o.</div>
+            <div>{Math.floor(petData.weight/1000)} kg</div>
+            <div>{petData.sex}</div>
+            <div>{petData.species}</div>
+            <div>{petData.breed}</div>
             <br />
-            {userData?.user?.isAdmin && (
+            <hr></hr>
+            {userData?.user?.isDoctor && (
                 <Link to="add">Add Manipulation</Link>
             )}
             <br />
             {manipulations.map(manipulation=>{
+                const {date, doctor, purpose} = manipulation;
+                const data = {date, doctor, purpose}
                 return(
-                    <Manipulation {...manipulation}></Manipulation>
+                    <Manipulation {...data}></Manipulation>
                 )
             })}
         </div>
