b0b33b4bd2096b158553d8b3e0b18cb4e240d7d4
Martin <moothreene@gmail.com>
2024-03-06 18:54:28 +0300

    user component view update (links)

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './User.css'
 
 function User({_id, email}) {
   return (
-    <div>
+    <div className='user_link'>
       <Link to={`/users/${_id}`}>{email}</Link>
     </div>
   )
