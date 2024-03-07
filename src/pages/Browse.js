9214b073e29a18ac46b7c59cc6e3c1ce3d8d6cfb
Martin <moothreene@gmail.com>
2024-03-07 20:41:22 +0300

    added search among users or pets

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -4,15 +4,23 @@ import axios from 'axios';
 import User from '../components/User';
 import { Navigate } from 'react-router-dom';
 import "./Browse.css"
+import PetBrowse from '../components/PetBrowse';
 
 function Browse() {
+  const[pets,setPets] = useState([]);
   const[users,setUsers] = useState([]);
+  const[filteredPets,setFilteredPets] = useState([]);
   const[filteredUsers,setFilteredUsers] = useState([]);
   const[searchQuery, setSearchQuery] = useState("");
   const userData = useSelector(state=>state.user);
+  const[searchSelection, setSearchSelection] = useState("pets");
 
   useEffect(()=>{
     if(userData.user){
+      axios.get("http://localhost:5000/pets",{withCredentials:true})
+      .then(response=>{
+        setPets(response.data);
+      })
       axios.get("http://localhost:5000/users",{withCredentials:true})
       .then(response=>{
         setUsers(response.data);
@@ -20,17 +28,37 @@ function Browse() {
     }
   },[]);
 
-  function searchUsers(term){
+  function search(term,target){
     if(term !==""){
-      return users.filter(user=>user.email.toLowerCase().includes(term.toLowerCase()));
+      if(target==="pets"){
+        return pets.filter(petData=>petDataContains(petData,term));
+      }
+      if(target==="clients"){
+        return users.filter(usersData=>userDataContains(usersData,term));
+      }
     }else{
       return [];
     }
-    
+  }
+
+  function petDataContains(data, term){
+    const result = (data?.name.toLowerCase().includes(term.toLowerCase())||
+      data?.owner_id?.firstName.toLowerCase().includes(term.toLowerCase())||
+      data?.owner_id?.lastName.toLowerCase().includes(term.toLowerCase()))
+    return result
+  }
+
+  function userDataContains(data, term){
+    const result = (data?.firstName.toLowerCase().includes(term.toLowerCase())||
+    data?.lastName.toLowerCase().includes(term.toLowerCase()))
+    return result
   }
 
   useEffect(() => {
-    const timeOutId = setTimeout(() => setFilteredUsers(searchUsers(searchQuery)), 300);
+    const timeOutId = setTimeout(() => {
+      setFilteredPets(search(searchQuery,"pets"))
+      setFilteredUsers(search(searchQuery,"clients"))
+    }, 300);
     return () => clearTimeout(timeOutId);
   }, [searchQuery]);
 
@@ -40,19 +68,44 @@ function Browse() {
   if (!userData.user.isDoctor) return <Navigate to={userData.user.id} />
   return (
     <div className='browse'>
-      <input 
+      <input
         className='search'
         value={searchQuery}
         placeholder='Search for users...'
-        onChange={event=>{setSearchQuery(event.target.value)
-        console.log("hey"+event.target.value+"hey")}}    
+        onChange={event=>{setSearchQuery(event.target.value)}}    
       />
+      <fieldset>
+        <input
+          type="radio"
+          className='pets'
+          id='pets'
+          checked={searchSelection==="pets"}
+          value={searchSelection}
+          onClick={()=>setSearchSelection("pets")}
+        />
+        <label for="pets">Pets</label>
+
+        <input 
+          type="radio"
+          className='clients'
+          id='clients'
+          checked={searchSelection==="clients"}
+          value={searchSelection}
+          onClick={()=>setSearchSelection("clients")}
+        />
+        <label for="clients">Clients</label>
+      </fieldset>
       <div className='clients_container'>
-        {
+        {searchSelection==="clients" && (
           filteredUsers.map(user=>{
             return(<User key={user._id} {...user} />)
           })
-        }
+        )}
+        {searchSelection==="pets" && (
+          filteredPets.map(pet=>{
+            return(<PetBrowse key={pet._id} {...pet} />)
+          })
+        )}
       </div>
     </div>
     
