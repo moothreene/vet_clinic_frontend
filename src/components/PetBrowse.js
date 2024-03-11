d3bedefdee666d36521d4d8d5994a2dc32f69fa3
Martin <moothreene@gmail.com>
2024-03-11 19:52:36 +0300

    search pet component view

--- src/components/PetBrowse.js
+++ src/components/PetBrowse.js
@@ -5,11 +5,12 @@ import "./PetBrowse.css"
 function PetBrowse({_id,owner_id,name,sex,weight,birthday,species,breed}) {
   const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
   return (
-    <div className="pet_link">
-        <Link to={`/users/${owner_id._id}/${_id}`}>
-            {name} {species} {sex} Owner: {owner_id.lastName} {owner_id.firstName}
-        </Link>
-    </div>
+    <tr className="pet_link">
+      <td><Link to={`/users/${owner_id._id}/${_id}`}>{name}</Link></td>
+      <td><Link to={`/users/${owner_id._id}/${_id}`}>{species}</Link></td>
+      <td><Link to={`/users/${owner_id._id}/${_id}`}>{sex}</Link></td>
+      <td className='owner'><Link to={`/users/${owner_id._id}/${_id}`}>Owner: {owner_id.lastName} {owner_id.firstName}</Link></td>
+    </tr>
   )
 }
 
