67f8d1d708df2532feb069090c46ef2e9115c7de
Martin <moothreene@gmail.com>
2024-03-11 19:51:44 +0300

    age calculation function update

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -3,6 +3,24 @@ import { Link } from 'react-router-dom';
 
 function Pet({_id,name,sex,weight,birthday,species,breed}) {
   const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+  function getAge(birthDate){
+    const today = new Date();
+    const birthDayDate = new Date(birthDate);
+    let age = today.getFullYear() - birthDayDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDayDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDayDate.getDate())) {
+        age--;
+    }
+    let result = "";
+    if(age > 0){
+        result += (age+" y.")
+    }
+    if(monthDiff<0){
+        result+= ((result?" ":"")+(12+monthDiff) + " m.")
+    }else if(monthDiff>0){
+        result += ((result?" ":"")+monthDiff + " m. ")
+    }
+    return result;
   return (
     <>
         <br />
@@ -13,6 +31,7 @@ function Pet({_id,name,sex,weight,birthday,species,breed}) {
         <div>{getAge(birthday)} y.o.</div>
         <div>{weight/1000} kg</div>
     </>
+        <td><Link to={_id}>{getAge(birthday)}o.</Link> </td>
   )
 }
 
--- src/pages/PetPage.js
+++ src/pages/PetPage.js
@@ -20,15 +20,33 @@ function PetPage() {
         )
         }
     },[]);
-
-    const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+    function getAge(birthDate){
+        const today = new Date();
+        const birthDayDate = new Date(birthDate);
+        let age = today.getFullYear() - birthDayDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDayDate.getMonth();
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDayDate.getDate())) {
+            age--;
+        }
+        let result = "";
+        if(age > 0){
+            result += (age+" y.")
+        }
+        if(monthDiff<0){
+            result+= ((result?" ":"")+(12+monthDiff) + " m.")
+        }else if(monthDiff>0){
+            result += ((result?" ":"")+monthDiff + " m. ")
+        }
+        return result;
+    }
+    
 
     if (!userData.user ||!(userData.user?.isDoctor || userData.user.id === id)) return <Navigate to="/" />
     return (
         <div>
             <Link to=".." relative="path">Owner:{petData.owner_id?.lastName} {petData.owner_id?.firstName}</Link>
             <h1>{petData.name}</h1>
-            <div>{getAge(petData.birthday)} y.o.</div>
+            <div>{getAge(petData.birthday)}o.</div>
             <div>{Math.floor(petData.weight/1000)} kg</div>
             <div>{petData.sex}</div>
             <div>{petData.species}</div>
