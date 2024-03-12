7c979cff8a9ed8ae47bd023e7d834489f32803f1
Martin <moothreene@gmail.com>
2024-03-12 15:55:43 +0300

    add and edit manipulation updated view

--- src/pages/AddManipulation.js
+++ src/pages/AddManipulation.js
@@ -2,16 +2,13 @@ import React, { useState } from 'react'
 import Editor from '../components/Editor'
 import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios';
+import './AddManipulation.css';
 
 function AddManipulation() {
-    const dateToday = new Date();
-    const dateText =dateToday.getFullYear()+"-"
-                +("0" + (dateToday.getMonth() + 1)).slice(-2) + "-" 
-                +("0" + dateToday.getDate()).slice(-2);
     const {petId} = useParams();
-    const [date,setDate] = useState(`${dateText}`);
-    const [weight,setWeight] = useState(0);
-    const [temp,setTemp] = useState(0);
+    const [date,setDate] = useState(`${dateText()}`);
+    const [weight,setWeight] = useState(null);
+    const [temp,setTemp] = useState(null);
     const [purpose,setPurpose] = useState("");
     const [desc, setDesc] = useState("");
     const [recommendation,setRecommendation] = useState("");
@@ -27,34 +24,51 @@ function AddManipulation() {
             }
         )
     }
+
+    function HandleInputSwitch(){
+        const inputFields = document.getElementsByClassName("quill");
+        const selectButtons = document.getElementsByClassName("select_button")
+        for(let inputField of inputFields){
+            inputField.classList.toggle("hidden")
+        }
+        for(let selectButton of selectButtons){
+            selectButton.classList.toggle("disabled");
+        }
+    }
+
+    function dateText(inputDate=new Date()){
+        const dateToday = new Date(inputDate);
+        const dateText =dateToday.getFullYear()+"-"
+                +("0" + (dateToday.getMonth() + 1)).slice(-2) + "-" 
+                +("0" + dateToday.getDate()).slice(-2)+" "
+                +("0" + dateToday.getHours()).slice(-2)+":"
+                +("0" + dateToday.getMinutes()).slice(-2);
+        return dateText;
+    }
+
     if(redirect) return navigate(-1)
     return (
         <div className='manipulation'>
             <form className="manipulationForm" onSubmit={HandleSubmit}>
-                <input type="date" required value={date} onChange={e=>{
-                    setDate(e.target.value)
-                    console.log(e.target.value)}}></input>
-                <br />
-                <label>Weight</label>
-                <br />
-                <input placeholder="Weight" type="number" value={weight} onChange={e=>setWeight(e.target.value)}></input>
-                <br />
-                <label>Temperature</label>
-                <br />
-                <input placeholder="Temperature" type="number" value={temp} onChange={e=>setTemp(e.target.value)}></input>
-                <br />
-                <label>Purpose</label>
-                <br />
-                <input placeholder="Purpose" type="text" required value={purpose} onChange={e=>setPurpose(e.target.value)}></input>
-                <br />
-                <label>Description</label>
-                <br />
-                <Editor value={desc} onChange={setDesc}/>
-                <br />
-                <label>Recommendation</label>
-                <br />
-                <Editor value={recommendation} onChange={setRecommendation}/>
-                <button type="submit">Submit</button>
+                <div className='manipulation_short'>
+                    <input type="datetime-local" required value={date} onChange={e=>{
+                        setDate(e.target.value)}}>
+                    </input>
+                    <input placeholder="Weight" type="number" value={weight} onChange={e=>setWeight(e.target.value)}></input>
+                    <input placeholder="Temperature" type="number" value={temp} onChange={e=>setTemp(e.target.value)}></input>
+                    <input placeholder="Purpose" type="text" required value={purpose} onChange={e=>setPurpose(e.target.value)}></input>
+                    <button type="submit">Submit</button>
+                </div>
+                <div className='manipulation_long'>
+                    <fieldset className='field desc'>
+                        <legend>
+                            <button type="button" className='desc select_button disabled' onClick={HandleInputSwitch}>Description</button>
+                            <button type="button" className='recommendation select_button' onClick={HandleInputSwitch}>Recommendation</button>
+                        </legend>
+                        <Editor value={desc} onChange={setDesc}/>
+                        <Editor className="hidden" value={recommendation} onChange={setRecommendation}/>
+                    </fieldset>
+                </div>
             </form>        
         </div>
     )
--- src/pages/EditManipulation.js
+++ src/pages/EditManipulation.js
@@ -4,15 +4,12 @@ import { Navigate, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios';
 
 function EditManipulation() {
-    const dateToday = new Date();
-    const dateText =dateToday.getFullYear()+"-"
-                +("0" + (dateToday.getMonth() + 1)).slice(-2) + "-" 
-                +("0" + dateToday.getDate()).slice(-2);
+    
     const {manipulationId} = useParams();
     const {petId} = useParams();
-    const [date,setDate] = useState({dateText});
-    const [weight,setWeight] = useState(0);
-    const [temp,setTemp] = useState(0);
+    const [date,setDate] = useState(`${dateText()}`);
+    const [weight,setWeight] = useState(null);
+    const [temp,setTemp] = useState(null);
     const [purpose,setPurpose] = useState("");
     const [desc, setDesc] = useState("");
     const [recommendation,setRecommendation] = useState("");
@@ -22,7 +19,7 @@ function EditManipulation() {
     useEffect(()=>{
         axios.get(`http://localhost:5000/manipulation/${manipulationId}`,{withCredentials:true}).then(
           response=>{
-            if(response.data?.date) setDate(response.data.date)
+            if(response.data?.date) setDate(dateText(response.data.date))
             if(response.data?.weight) setWeight(response.data.weight)
             if(response.data?.temp) setTemp(response.data.temp)
             if(response.data?.purpose) setPurpose(response.data.purpose)
@@ -50,33 +47,51 @@ function EditManipulation() {
         )
     }
 
+    function dateText(inputDate=new Date()){
+        const dateToday = new Date(inputDate);
+        const dateText =dateToday.getFullYear()+"-"
+                +("0" + (dateToday.getMonth() + 1)).slice(-2) + "-" 
+                +("0" + dateToday.getDate()).slice(-2)+" "
+                +("0" + dateToday.getHours()).slice(-2)+":"
+                +("0" + dateToday.getMinutes()).slice(-2);
+        return dateText;
+    }
+
+    function HandleInputSwitch(){
+        const inputFields = document.getElementsByClassName("quill");
+        const selectButtons = document.getElementsByClassName("select_button")
+        for(let inputField of inputFields){
+            inputField.classList.toggle("hidden")
+        }
+        for(let selectButton of selectButtons){
+            selectButton.classList.toggle("disabled");
+        }
+    }
+
     if(redirect) return navigate(-1);
     return (
         <div className='manipulation'>
-            <button onClick={HandleDelete}>Delete</button>
             <form className="manipulationForm" onSubmit={HandleSubmit}>
-                <input type="date" required value={date} onChange={e=>setDate(e.target.value)}></input>
-                <br />
-                <label>Weight</label>
-                <br />
-                <input placeholder="Weight" type="number" value={weight} onChange={e=>setWeight(e.target.value)}></input>
-                <br />
-                <label>Temperature</label>
-                <br />
-                <input placeholder="Temperature" type="number" value={temp} onChange={e=>setTemp(e.target.value)}></input>
-                <br />
-                <label>Purpose</label>
-                <br />
-                <input placeholder="Purpose" type="text" required value={purpose} onChange={e=>setPurpose(e.target.value)}></input>
-                <br />
-                <label>Description</label>
-                <br />
-                <Editor value={desc} onChange={setDesc}/>
-                <br />
-                <label>Recommendation</label>
-                <br />
-                <Editor value={recommendation} onChange={setRecommendation}/>
-                <button type="submit">Submit</button>
+                <div className='manipulation_short'>
+                    <input type="datetime-local" required value={date} onChange={e=>{
+                        setDate(e.target.value)}}>
+                    </input>
+                    <input placeholder="Weight" type="number" value={weight} onChange={e=>setWeight(e.target.value)}></input>
+                    <input placeholder="Temperature" type="number" value={temp} onChange={e=>setTemp(e.target.value)}></input>
+                    <input placeholder="Purpose" type="text" required value={purpose} onChange={e=>setPurpose(e.target.value)}></input>
+                    <button type="submit">Edit</button>
+                    <button onClick={HandleDelete}>Delete</button>
+                </div>
+                <div className='manipulation_long'>
+                    <fieldset className='field desc'>
+                        <legend>
+                            <button type="button" className='desc select_button disabled' onClick={HandleInputSwitch}>Description</button>
+                            <button type="button" className='recommendation select_button' onClick={HandleInputSwitch}>Recommendation</button>
+                        </legend>
+                        <Editor value={desc} onChange={setDesc}/>
+                        <Editor className="hidden" value={recommendation} onChange={setRecommendation}/>
+                    </fieldset>
+                </div>
             </form>        
         </div>
     )
