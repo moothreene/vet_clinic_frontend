ab46305543285fd43ee7934d27fb08052b5a871e
Martin <moothreene@gmail.com>
2024-03-12 15:57:57 +0300

    adding time and changing view of manipulation component

--- src/components/Manipulation.js
+++ src/components/Manipulation.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Manipulation.css';
+
 function Manipulation({_id,date,doctor, purpose}) {
+  function getDate(){
+    let d = new Date(date);
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    let year = d.getFullYear();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return {
+      "date":[year, month, day].join('-'),
+      "time":[hour, minute].join(':')
+    }
+  }
   return (
-    <Link to={_id}>
-      <div>
-        <hr></hr>
-        <div className='date'>{date}</div>
-        <div className='doctor'>Dr. {doctor.firstName} {doctor.lastName}</div>
-        <div className='purpose'>{purpose}</div>
-        <hr></hr>
-      </div>
-    </Link>
+      <tr className='manipulation_preview'>
+        <td className='date'><Link to={_id}>{getDate().date}</Link></td>
+        <td className='time'><Link to={_id}>{getDate().time}</Link></td>
+        <td className='purpose'><Link to={_id}>{purpose}</Link></td>
+        <td className='doctor'><Link to={_id}>Dr. {doctor.firstName} {doctor.lastName}</Link></td>
+      </tr>
     
   )
 }
