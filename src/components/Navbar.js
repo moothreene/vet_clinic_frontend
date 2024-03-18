cdf4a418c8d73ba679930acd852822e5bffb4d09
Martin <moothreene@gmail.com>
2024-03-19 00:22:15 +0300

    navbar rework

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react'
-import { Link, Navigate } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import { useSelector,useDispatch } from 'react-redux'
 import { logoutUser } from '../redux'
 import { useState } from 'react'
 import { clearError,updateUser } from '../redux'
 import './Navbar.css';
+import { serverUrl } from '../Utils'
 
 function Navbar() {
   const dispatch = useDispatch();
   const userData = useSelector(state=>state.user);
+  const [location,setLocation] = useState(window.location.href);
   const [redirect,setRedirect] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(()=>{
     dispatch(clearError());
@@ -18,6 +21,7 @@ function Navbar() {
 
   useEffect(()=>{
     setRedirect(false);
+    setLocation(window.location.href)
   })
 
   function logout(){
@@ -33,21 +37,29 @@ function Navbar() {
         </li>
       ):
         (<>
+          {location !== (window.location.origin+"/") && (
+            <li className='go_back'>
+              <div onClick={()=>{navigate(-1)}}>
                 <img className="img_default" src={require(`../images/return.png`)} alt="return"/>
                 <img className="img_hover" src={require(`../images/return_reverse.png`)} alt="return"/>
+              </div>
+            </li>
+          )}
           {userData.user.isDoctor &&
             <li className="register">
               <Link className="link register" to="/register">Register</Link>
             </li>
           }
           <li>
-            <Link className="link users" to="/users">{userData.user.email}</Link>
-          </li>
-          <li>
             <div className="link logout" onClick={logout}>Logout</div>
           </li>
+          <li><span>{userData.user.email}</span></li>
+          <li className='search_link'>
+            <Link className="link users" to="/users">
               <img className="img_default" src={require(`../images/search.png`)} alt="return"/>
                 <img className="img_hover" src={require(`../images/search_reverse.png`)} alt="return"/>
+            </Link>
+          </li>
         </>)}       
     </ul>
   )
